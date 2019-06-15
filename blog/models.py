4e3affcbc92af595483242e5a71de6d8fc8d14a2
Xiao Xiao <xiao.xiao@us.abb.com>
2019-06-14 18:17:56 -0700

    Added main category page

--- blog/models.py
+++ blog/models.py
@@ -18,18 +18,14 @@ class Category(Enum):
   Harassment = 'Harassment'; 
   Discrimination = 'Discrimination'; 
   Politics = 'Politics';
-  Worklife = 'Worklife';
   Conflict = 'Conflict';
-  ConflictEM = 'ConflictEM' 
   Miscellaneous = 'Miscellaneous';
   
   class Labels:
     Discrimination = 'Discrimination'; 
-    Harassment = 'Sexual Harassment';
+    Harassment = 'Harassment';
     Politics = 'Politics';
-    Conflict = 'Conflict Between Peers';
-    ConflictEM = 'Employee-Manager Conflict'
-    Worklife = 'Worklife Imbalance';
+    Conflict = 'Employee Conflicts';
     Miscellaneous = 'Other issues';
     
   def get_label(cat_name):
@@ -44,13 +40,7 @@ class Category(Enum):
       
       if cat_name == "Conflict":
         return Category.Conflict
-        
-      if cat_name =="ConflictEM":
-        return Category.ConflictEM
-        
-      if cat_name == "Worklife":
-        return Category.Worklife
-      
+            
       if cat_name == "Miscellaneous":
         return Category.Miscellaneous
     
